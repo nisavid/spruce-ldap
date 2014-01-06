39137c163d6ce7aaf19a670f5c1772a3dd7f487a
Ivan D Vasin <ivan@pixability.com>
2014-01-06 10:24:19 -0500

    update copyrights

--- spruce/ldap/_users.py
+++ spruce/ldap/_users.py
@@ -1,6 +1,6 @@
 """Users."""
 
-__copyright__ = "Copyright (C) 2013 Ivan D Vasin and Cogo Labs"
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
 __docformat__ = "restructuredtext"
 
 from collections import namedtuple as _namedtuple
--- spruce/ldap/openldap/__init__.py
+++ spruce/ldap/openldap/__init__.py
@@ -5,7 +5,7 @@ These objects provide support for service implementations based on
 
 """
 
-__copyright__ = "Copyright (C) 2013 Ivan D Vasin and Cogo Labs"
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
 __docformat__ = "restructuredtext"
 
 import os as _os
