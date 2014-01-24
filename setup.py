1b1d7fa677fd3fa129fa6c254df73706ac121521
Ivan D Vasin <ivan@pixability.com>
2014-01-24 15:21:07 -0500

    project metadata: add download URI

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ VERSION = '0.1.2'
 
 SITE_URI = ''
 
+DOWNLOAD_URI = 'https://github.com/nisavid/spruce-ldap'
+
 DESCRIPTION = 'Directory service interaction via LDAP'
 
 README_FILE = 'README.rst'
@@ -91,6 +93,7 @@ if __name__ == '__main__':
     _setup(name=NAME,
            version=VERSION,
            url=SITE_URI,
+           download_url=DOWNLOAD_URI,
            description=DESCRIPTION,
            long_description=LONG_DESCRIPTION,
            author=', '.join(__credits__),
